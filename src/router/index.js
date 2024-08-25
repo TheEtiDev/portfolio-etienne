341059b79d4fedf42ccf4b086307f8c6288dd1eb
Étienne Lessard <131507121+ETI-DEV@users.noreply.github.com>
2024-08-25 15:57:12 -0400

    fix page projet not found

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import EventlitProjetView from '../views/EventlitProjetView.vue'
+import CreditsView from '../views/CreditsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,12 +14,12 @@ const router = createRouter({
     {
       path: '/credits',
       name: 'credits',
-      component: () => import('../views/CreditsView.vue')
+      component: CreditsView
     },
     {
       path: '/eventlit',
       name: 'eventlit',
-      component: () => import('../views/EventlitProjetView.vue')
+      component: EventlitProjetView
     }
     // {
     //   path: '/about',
